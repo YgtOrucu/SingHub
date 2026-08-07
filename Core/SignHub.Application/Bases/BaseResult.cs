6f28881f609a86224c5d7f8fa1ca2ecc1d9716d3
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2026-08-07 22:56:26 +0300

    .

---
+++ Core/SignHub.Application/Bases/BaseResult.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace SignHub.Application.Bases;
+
+public class BaseResult<T>
+{
+    public T? Data { get; set; }
+    public IEnumerable<Error>? Errors { get; set; }
+
+    [JsonIgnore]
+    public bool IsSuccess => Errors != null && Errors.Any();
+
+    [JsonIgnore]
+    public bool IsFailure => !IsSuccess;
+
+
+    public static BaseResult<T> Success(T data)
+    {
+        return new BaseResult<T> { Data = data };
+    }
+
+    public static BaseResult<T> Failure(string message)
+    {
+        return new BaseResult<T> { Errors = [new Error { Message = message }] };
+    }
+
+    public static BaseResult<T> Failure()
+    {
+        return new BaseResult<T> { Errors = [new Error { Message = "An Error Occurred" }] };
+    }
+
+    public static BaseResult<T> Failure(IEnumerable<string> errors)
+    {
+        return new BaseResult<T> { Errors = errors.Select(e => new Error { Message = e }) };
+    }
+
+    public static BaseResult<T> Failure(IEnumerable<IdentityError> errors)
+    {
+        return new BaseResult<T> { Errors = errors.Select(e => new Error { Code = e.Code, Message = e.Description }) };
+    }
+
+}
+public class Error
+{
+    public string? Code { get; set; }
+    public string? Message { get; set; }
+}
--- Presentation/SignHub.WebAPI/Program.cs
+++ Presentation/SignHub.WebAPI/Program.cs
@@ -1,12 +1,12 @@
 using Scalar.AspNetCore;
 using SignHub.Persistence.Extensions;
+using SignHub.WebAPI.CustomMiddlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AppPersistenceSetting(builder.Configuration);
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -17,9 +17,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-
+app.UseMiddleware<CustomExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
